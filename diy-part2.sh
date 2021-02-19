0feaa5ee143d13f19c887175ebc42d1816c5440e
Lee <dyingway@gmail.com>
2021-02-19 10:10:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,11 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/fw876/helloworld.git package/gfwdown/ssrp
-git clone https://github.com/xiaorouji/openwrt-passwall package/gfwdown/passwall
-
-mkdir -p package/gfwdown/redsocks2
-wget https://raw.githubusercontent.com/coolsnowwolf/lede/a0ff7c025aaea37ccbb382d9f45ebe1b77c35b4b/package/lean/redsocks2/Makefile -O package/gfwdown/redsocks2/Makefile
 
 mkdir -p tools/ucl 
 wget https://raw.githubusercontent.com/Lienol/openwrt/af6e1632cd9250c0b4dc47606c1eb92ba4b3ae81/tools/ucl/Makefile -O tools/ucl/Makefile
@@ -26,5 +21,13 @@ mkdir -p tools/upx
 wget https://raw.githubusercontent.com/Lienol/openwrt/af6e1632cd9250c0b4dc47606c1eb92ba4b3ae81/tools/upx/Makefile -O tools/upx/Makefile
 
 #添加upx到工具makefile中
-sed -i '$ a tools-y += ucl upx' tools/Makefile
-sed -i '$ a $(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
+sed -i '25 a tools-y += ucl upx' tools/Makefile
+sed -i '55 a $(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
+
+
+
+git clone https://github.com/fw876/helloworld.git package/gfwdown/ssrp
+git clone https://github.com/xiaorouji/openwrt-passwall package/gfwdown/passwall
+
+mkdir -p package/gfwdown/redsocks2
+wget https://raw.githubusercontent.com/coolsnowwolf/lede/a0ff7c025aaea37ccbb382d9f45ebe1b77c35b4b/package/lean/redsocks2/Makefile -O package/gfwdown/redsocks2/Makefile
